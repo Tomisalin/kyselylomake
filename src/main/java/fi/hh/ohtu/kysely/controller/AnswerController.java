25b4c6a8a6e1185eb9a65061c99d349ac8c8f484
joelvepsalainen <joel.vepsalainen@myy.haaga-helia.fi>
2017-05-04 23:31:46 +0300

    answercontrollerjuduu

--- src/main/java/fi/hh/ohtu/kysely/controller/AnswerController.java
+++ src/main/java/fi/hh/ohtu/kysely/controller/AnswerController.java
@@ -4,14 +4,16 @@ package fi.hh.ohtu.kysely.controller;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.hh.ohtu.kysely.bean.Answer;
+import fi.hh.ohtu.kysely.bean.AnswerImpl;
 import fi.hh.ohtu.kysely.dao.AnswerDAO;
-
 
 @Controller
 @RequestMapping (value="/vastaukset")
@@ -28,16 +30,19 @@ public class AnswerController {
 		this.dao = dao;
 	}
 
-	@RequestMapping(value="vastaus", method = RequestMethod.POST)
+	/*@RequestMapping(value="vastaus", method = RequestMethod.POST)
 	public Answer saveAnswer(@RequestBody Answer a) {
 	
 		Answer answer = dao.saveAnswer(a);
 	
 		return answer;
+	}*/
+	
+	@RequestMapping(value="vastaus", method=RequestMethod.POST)
+	public String create( @ModelAttribute(value="answer") AnswerImpl answer) {
+		dao.saveAnswer(answer);
+		return "kysely/lomake";
 	}
-	
-	
-	
 	
 
 }
