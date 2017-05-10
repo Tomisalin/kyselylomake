2fdf9289b182614963864b16bfe97706be60b507
Tontti Matias <a1402939@myy.haaga-helia.fi>
2017-05-10 17:23:36 +0300

    Insert sql

--- database/insert_into.sql
+++ database/insert_into.sql
@@ -1,26 +1,32 @@
 INSERT INTO topic
 	(topic_name)
 VALUES 
-	('Ruokailu');
+	('Kampus');
 	
 INSERT INTO topic
 	(topic_name)
 VALUES
-	('Ainejärjestö');
+	('Koulutus');
+	
+INSERT INTO topic
+	(topic_name)
+VALUES
+	('Opiskelijaelämä');
 
 INSERT INTO survey 
 	(survey_id, survey_name , topic_name)
 VALUES
-	(1,'Ruokailu toukokuussa 2017','Ruokailu');
+	(1,'Kampus kysely 2017','Kampus');
 	
 INSERT INTO survey 
 	(survey_id, survey_name , topic_name)
 VALUES
-	(2,'Ainejärjestö esimerkki kysely','Ainejärjestö');
+	(2,'Koulutus kysely 2017','Koulutus');
 
-INSERT INTO question_type
-	(type_name)
-VALUES ('multichoice');
+INSERT INTO survey 
+	(survey_id, survey_name , topic_name)
+VALUES
+	(3,'Opiskelija kysely 2017','Opiskelijaelämä');
 
 INSERT INTO question_type
 	(type_name)
@@ -34,53 +40,113 @@ INSERT INTO question_type
 	(type_name)
 VALUES ('onetofive');
 
+INSERT INTO question_type
+	(type_name)
+VALUES ('frequency');
 
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
-VALUES (1,1, 'Textbox', 'Arvioi viikon ruoat omin sanoin');
+VALUES (1,1, 'yesno', 'Oletko tietoinen kouluterveydenhoitoon kuuluvista ihmisistä? ( Koulupsykologi, Terveydenhuolto, Pappi, Kuraattori)');
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
-VALUES (2,1, 'yesno', 'Oliko ruoka hyvää tänään?');
+VALUES (2,1, 'yesno', 'Pitäisikö näistä antaa lisätietoja opiskelijoille?');
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
-VALUES (3,1, 'onetofive', 'Arvioi tiistain ruoka (1-5)');
+VALUES (3,1, 'yesno', 'Oletko käyttänäyt Helga Kitchen:ä?');
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
-VALUES (4,2, 'yesno', 'Tekevätkö ainejärjestöt hyvää työtä?');
+VALUES (4,1, 'yesno', 'Olivatko tilat siistit?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (5,1, 'textbox', 'Miten parantaisit tiloja?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (6,1, 'yesno', 'Ovatko yleiset tilat mielestäsi viihtyisät?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (7,1, 'yesno', 'Onko yleisissä tiloissa tarpeeksi istumatilaa?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (8,1, 'yesno', 'Toimivatko yleiset tietokoneet mielestäsi hyvin?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (9,1, 'yesno', 'Oletko tyytyväinen opettajiin ja opetukseen?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (10,1, 'yesno', 'Oletko ollut yhteydessä opoon liittyen opintoihisi?');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (11,1, 'yesno', 'Oletko tyytyväinen muuhun henkilökuntaan? ( Siistijä, aulatyöntekijä, IT- tuki jne.)');
+
+INSERT INTO question 
+	(question_id, survey_id, type_name, question)
+VALUES (12,1, 'textbox', 'Toiveita tai palautetta henkilökuntaa koskien:');
+
+
+
 
 
 
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (1,'yesno','Kyllä');
+VALUES (1,'textbox','Ei vaihtoehtoja');
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (2, 'yesno','Ei');
+VALUES (2, 'yesno','Kyllä');
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (3, 'onetofive',1);
+VALUES (3, 'yesno','Ei');
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (4, 'onetofive',2);
+VALUES (4, 'onetofive',1);
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (5, 'onetofive',3);
+VALUES (5, 'onetofive',2);
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (6, 'onetofive',4);
-
+VALUES (6, 'onetofive',3);
 
 INSERT INTO option_choice
 	(option_id, type_name, optionchoice)
-VALUES (7, 'onetofive',5);
+VALUES (7, 'onetofive',4);
+
+INSERT INTO option_choice
+	(option_id, type_name, optionchoice)
+VALUES (8, 'frequency', 'Usein');
+
+INSERT INTO option_choice
+	(option_id, type_name, optionchoice)
+VALUES (9, 'frequency', 'Joskus');
+
+INSERT INTO option_choice
+	(option_id, type_name, optionchoice)
+VALUES (10, 'frequency', 'Harvoin');
+
+INSERT INTO option_choice
+	(option_id, type_name, optionchoice)
+VALUES (11, 'frequency', 'En ole käynyt');
+
+
+
+
+
+
+
 
