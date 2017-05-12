917948bd9cc48dff15c55989fa2f6f0dc53fd541
Salin Tomi <a1600563@myy.haaga-helia.fi>
2017-05-12 13:30:32 +0300

    Filtteri lisätty

--- database/insert_into.sql
+++ database/insert_into.sql
@@ -12,6 +12,11 @@ INSERT INTO topic
 	(topic_name)
 VALUES
 	('Opiskelijaelämä');
+	
+INSERT INTO topic
+	(topic_name)
+VALUES
+	('Oma palaute');
 
 INSERT INTO survey 
 	(survey_id, survey_name , topic_name)
@@ -27,6 +32,11 @@ INSERT INTO survey
 	(survey_id, survey_name , topic_name)
 VALUES
 	(3,'Opiskelija kysely 2017','Opiskelijaelämä');
+	
+INSERT INTO survey 
+	(survey_id, survey_name , topic_name)
+VALUES
+	(4,'Anna oma palaute','Oma palaute');	
 
 INSERT INTO question_type
 	(type_name)
@@ -79,11 +89,7 @@ VALUES (8,1, 'yesno', 'Toimivatko yleiset tietokoneet mielestäsi hyvin?');
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
-<<<<<<< HEAD
-VALUES (1,1, 'textbox', 'Arvioi viikon ruoat omin sanoin');
-=======
 VALUES (9,1, 'yesno', 'Oletko tyytyväinen opettajiin ja opetukseen?');
->>>>>>> 2fdf9289b182614963864b16bfe97706be60b507
 
 INSERT INTO question 
 	(question_id, survey_id, type_name, question)
@@ -98,9 +104,105 @@ INSERT INTO question
 VALUES (12,1, 'textbox', 'Toiveita tai palautetta henkilökuntaa koskien:');
 
 
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (13, 2, 'yesno', 'Oletko tyytyväinen tutkintovalintaasi?');
 
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (14, 2, 'yesno', 'Oletko saanut tarpeeksi tietoa profiiliopinnoistasi?');
 
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (15, 2, 'yesno', 'Oletko saanut tarpeeksi tietoa valinnaisista kursseista?');
 
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (16, 2, 'yesno', 'Onko opiskelutahti mielestäsi sopiva aikautaulussa valmistumiseen?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (17, 2, 'yesno', 'Antavatko opinnot mielestäsi riittävät valmiudet työelämään?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (18, 2, 'textbox', 'Haluatko antaa palautetta ylläoleviin kysymyksiin liittyen:');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (19, 2, 'yesno', 'Ovatko nämä aiheet tuttuja sinulle: Opinnäytetyö, harjoittelu ja vaihto?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (20, 2, 'yesno', 'Saatko näistä tarpeeksi tietoa?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (21, 2, 'textbox', 'Haluaisitko jostakin ylläolevista lisätietoa/ mietityttääkö jokin näistä?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (22, 2, 'yesno', 'Onko opiskelumateriaali helposti saatavilla ja ajan tasalla?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (23, 2, 'yesno', 'Ovatko opiskelumenetelmät monipuolisia? ( Esseet, tehtävät, ryhmätyöt jne.)');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (24, 2, 'yesno', 'Saatko yhteyden opettajiin helposti?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (25, 2, 'yesno', 'Tulevatko arvosanat ajallaan?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (26, 2, 'yesno', 'Koetko oppivasi tunneilla ja tehtävien kautta?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (27, 2, 'onetofive', 'Arvioi opiskeluihin liittyvät sivut asteikolla 1-5: Mynet, WinhaWille, Moodle');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (28,3, 'yesno', 'Oletko tietoinen opiskelijaeduista ja opiskelijakortin hankkimisesta?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (29, 3, 'yesno', 'Tiedätkö mitä edunvalvonta sisältää ja kuka sitä hoitaa?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (30,3, 'textbox', 'Koetko edustajiston ajavan opiskelijoiden etua?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (31, 3, 'yesno', 'Jos olet lähettänyt jäsenaloitteen, onko siihen vastattu?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (32, 3, 'textbox', 'Oletko tietoinen: kerhoista, liikuntaryhmistä ja kirjastosta?');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (33, 3, 'frequency', 'Valitse sopivin vaihtoehto, kuinka usein/ oletko käynyt: Oman ainejärjestön tapahtumissa');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (34, 3, 'frequency', 'Muiden ainejärjestöjen tai koulujen tapahtumissa');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (35, 3, 'frequency', 'Tunnet kuuluvasi "porukkaan');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (36, 3, 'frequency', 'Löydän tietoa tapahtumista helposti');
+
+INSERT INTO question
+	(question_id, survey_id, type_name, question)
+VALUES (37, 4, 'textbox', 'Haluatko jättää jotakin muuta palautetta? Kirjoita tänne viestisi!');
 
 
 INSERT INTO option_choice
